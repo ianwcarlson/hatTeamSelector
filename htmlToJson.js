68658e56f5cba0df6c1563e6b881fb863abf9eec
ianwcarlson <ianwcarlson@gmail.com>
2014-11-19 23:29:19 -0700

    successfully parsed html file into javavascript data structure

--- htmlToJson.js
+++ htmlToJson.js
@@ -2,7 +2,7 @@
 	var htmlparser = require("htmlparser");
 	var fs = require('fs');
 	var rawHtml = fs.readFileSync('./data/formResponses1.html');
-	debugger;
+
 
 
 	var handler = new htmlparser.DefaultHandler(function (error, dom) {
@@ -16,7 +16,7 @@
 	parser.parseComplete(rawHtml);
 
 	var inputArray = [];
-	var inputRow = {};
+	var inputRow = [];
 	var headerRowArray = handler.dom[4].children[0].children[1].children[0].children;
 
 	for (i=1; i<headerRowArray.length; i++){
@@ -24,9 +24,28 @@
 		if (prefix.type === "tag"){
 			prefix = headerRowArray[i].children[0].children[0];
 		} 
-		inputRow[prefix.data] = '';
+		inputRow.push(prefix.data);
 	}
 
-	
+	var tableArray = handler.dom[4].children[0].children[1].children;
+	for (rowIdx = 2; rowIdx<tableArray.length; rowIdx++){
+		var rowArray = tableArray[rowIdx].children;
+		var newRowObject = {};
+		for (cellIdx=1; cellIdx<rowArray.length; cellIdx++){
+			if (typeof rowArray[cellIdx].children !== 'undefined'){
+				prefix = rowArray[cellIdx].children[0];
+				if (prefix.type === "tag"){
+					prefix = rowArray[cellIdx].children[0].children[0];
+				}
+				newRowObject[inputRow[cellIdx-1]] = prefix.data;
+				//console.log('cell contents: ', prefix.data);
+			} else {
+				newRowObject[inputRow[cellIdx-1]] = '';
+			}
+		}
+		inputArray.push(newRowObject);
+		
+	}
+	debugger;		
 	//sys.puts(sys.inspect(handler.dom, false, null));
 //}
