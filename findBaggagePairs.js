73b96f6b7e138703e62f9a3ac5d7452e75f0a1da
ianwcarlson <ianwcarlson@gmail.com>
2014-11-20 21:13:19 -0700

    doesn't compile. successfully finding pairs but need to deal with shrinking list

--- findBaggagePairs.js
+++ findBaggagePairs.js
@@ -2,12 +2,42 @@ module.exports = function(inputPlayerList){
 	var underscore = require('underscore');
 
 	var baggageList = underscore.reject(inputPlayerList, function(listItem){
-		if (listItem['Name of Baggage Player (person you want to play with)'] === ''){
+		if (listItem.baggage === ''){
 			return true;
 		} else {
 			return false;
 		}
 	});
+
+	underscore.each(baggageList, function(element, index, list){
+		element.baggage = element.baggage.toLowerCase();
+		element.firstName = element.firstName.toLowerCase();
+		element.lastName = element.lastName.toLowerCase();	
+	})
+
+	var baggagePairs = [];
+	//for (var i=0; i<baggageList.length; i++){
+	underscore.each(baggageList, function(element, index, list){
+		//var lowerBagPerson = element.baggage.toLowerCase();
+		var lowerBagPersonList = element.baggage.split(" ");
+		var firstName = lowerBagPersonList[0];
+		var lastName = lowerBagPersonList[1];
+		var foundList = underscore.where(baggageList, {lastName: lastName});
+
+		if (foundList.length === 1){
+			// now check to see if baggage pair has already been found
+			var foundIdx = underscore.indexOf(baggageList, foundList[0]);
+			baggageList.splice(foundIdx, 1);
+			baggageList.splice(index, 1);
+
+			baggagePairs.push([element, foundList[0]])
+			
+		} else {
+			console.log('Didnt find ', lastName, ' in baggage list');
+		}	
+	})
+		
+	//}
 
 	debugger;
 
