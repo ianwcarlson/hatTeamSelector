7ecaf87e2b2842ab89010cfbb1a8f2703a67aa21
ianwcarlson <ianwcarlson@gmail.com>
2014-11-21 22:41:12 -0700

    super hacked up version that barely works.  csv formatting needs work.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var underscore = require('underscore');
-
+var fs = require('fs');
 var parser = require('./htmlToJson.js');
 var parsedList = parser('./data/formResponses2.html');
 
@@ -59,11 +59,10 @@ underscore.each(sortedBaggageList, function(element, index, list){
 	selectNewTeam.selectNewTeam();
 })
 selectNewTeam.resetTeamIndex();
-debugger;
 
-
+/// FEMALES!!!!
 // start loading up the team lists with females
-while (remainingFemalePlayers.length > 0 || !areSideBarsEmpty()){ // may need to count sideboard to make sure it's empty
+while (remainingFemalePlayers.length > 0 || !areSideBarsEmptyFemale()){ // may need to count sideboard to make sure it's empty
 	//underscore.each(outerElement, function(innerElement, innerIndex, list){
 	var idx = selectNewTeam.getTeamSelect();
 	// compare side board to remaining list
@@ -88,7 +87,7 @@ while (remainingFemalePlayers.length > 0 || !areSideBarsEmpty()){ // may need to
 
 selectNewTeam.resetTeamIndex();
 //check each side board and make sure they're empty
-function areSideBarsEmpty(){
+function areSideBarsEmptyFemale(){
 	var empty = true;
 	underscore.each(sideBar2DList, function(element, index, list){
 		if (element[0].female.length !== 0){
@@ -97,7 +96,61 @@ function areSideBarsEmpty(){
 	})
 	return empty;
 }
-debugger;
+
+/// MALES!!!!
+// start loading up the team lists with females
+while (remainingMalePlayers.length > 0 || !areSideBarsEmptyMale()){ // may need to count sideboard to make sure it's empty
+	//underscore.each(outerElement, function(innerElement, innerIndex, list){
+	var idx = selectNewTeam.getTeamSelect();
+	// compare side board to remaining list
+	var sideboardArray = sideBar2DList[idx][0].male;
+	if (sideboardArray.length === 0 || (remainingMalePlayers.length > 0 && 
+		remainingMalePlayers[0].skill > sideboardArray[0].skill)){
+		if (typeof remainingMalePlayers[0] !== 'undefined'){
+			team2DList[idx].push(remainingMalePlayers[0]);
+			remainingMalePlayers = remainingMalePlayers.slice(1);
+		}
+	} 
+	else if (remainingMalePlayers.length === 0 || 
+		(sideboardArray.length > 0 && remainingMalePlayers[0].skill <= sideboardArray[0].skill)){
+		if (typeof sideboardArray[0] !== 'undefined'){
+			team2DList[idx].push(sideboardArray[0]);
+			sideBar2DList[idx][0].male = sideBar2DList[idx][0].male.slice(1);
+		}
+	} 
+	
+	selectNewTeam.selectNewTeam();
+}
+
+selectNewTeam.resetTeamIndex();
+//check each side board and make sure they're empty
+function areSideBarsEmptyMale(){
+	var empty = true;
+	underscore.each(sideBar2DList, function(element, index, list){
+		if (element[0].male.length !== 0){
+			empty = false;
+		}	
+	})
+	return empty;
+}
+
+var totalCsv = '';
+var json2csv = require('json2csv');
+underscore.each(team2DList, function(element, index, list){
+	json2csv({data: element, fields: ['firstName', 'lastName', 'skill']}, function(err, csv) {
+	  if (err) console.log(err);
+	  totalCsv += csv + '\r\n\r\n';
+	  //fs.writeFile('file' + index + '.csv', csv, function(err) {
+	  //  if (err) throw err;
+	  //  console.log('file saved');
+	  //});
+	});	
+})
+
+fs.writeFile('Teams.csv', totalCsv, function(err) {
+	    if (err) throw err;
+	    console.log('file saved');
+	  });
 
 function divideGender(team, player){
 	if (player.gender === 'female'){
@@ -181,5 +234,3 @@ function distillPlayerList(rawPlayerList){
 // insert baggage players
 
 // start add women to team list
-
-debugger;
