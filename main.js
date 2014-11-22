937c8344dbc47d368c33260bdbf3e53c361e6802
ianwcarlson <ianwcarlson@gmail.com>
2014-11-21 20:01:28 -0700

    got girl selection working ok

--- main.js
+++ main.js
@@ -30,18 +30,13 @@ underscore.each(sortedBaggageList, function(element, index, list){
 
 // sort by skill level in descending order
 var remainingPlayerList = underscore.difference(conditionedPlayerList, flattenedBaggageList);
-var sortedRemainingPlayerlist = underscore.sortBy(remainingPlayerList, function(item){
+var sortedRemainingPlayerList = underscore.sortBy(remainingPlayerList, function(item){
 	return item.skill;
 });
-sortedRemainingPlayerlist.reverse();
+sortedRemainingPlayerList.reverse();
 
-var remainingFemalePlayers = underscore.where(sortedRemainingPlayerlist, function(item){
-	item.gender === 'female';
-});
-var remainingMalePlayers = underscore.where(sortedRemainingPlayerlist, function(item){
-	item.gender === 'male';
-});
-
+var remainingFemalePlayers = underscore.where(sortedRemainingPlayerList, {'gender': 'female'});
+var remainingMalePlayers = underscore.where(sortedRemainingPlayerList, {'gender': 'male'});
 // initialize team lists
 var numTeams = 8;
 var team2DList = [];
@@ -50,24 +45,59 @@ for (var i=0; i<numTeams; i++){
 }
 
 // fill up team side bars
-var sideBar2DList = team2DList;
+var sideBar2DList = [];
+for (var i=0; i<numTeams; i++){
+	sideBar2DList.push([]);
+}
+underscore.each(sideBar2DList, function(element, idx, list){
+	element.push({'female': [], 'male': []});
+})
 underscore.each(sortedBaggageList, function(element, index, list){
 	var idx = selectNewTeam.getTeamSelect();
-	sideBar2DList[idx].push({'female': [], 'male': []});
 	divideGender(sideBar2DList[idx][0], element[0]);
 	divideGender(sideBar2DList[idx][0], element[1]);
 	selectNewTeam.selectNewTeam();
 })
-
+selectNewTeam.resetTeamIndex();
 debugger;
 
-// start loading up the team lists
-underscore.each(team2DList, function(outerElement, index, list){
-	underscore.each(outerElement, function(innerElement, index, list){
 
-	}
-})
+// start loading up the team lists with females
+while (remainingFemalePlayers.length > 0 || !areSideBarsEmpty()){ // may need to count sideboard to make sure it's empty
+	//underscore.each(outerElement, function(innerElement, innerIndex, list){
+	var idx = selectNewTeam.getTeamSelect();
+	// compare side board to remaining list
+	var sideboardArray = sideBar2DList[idx][0].female;
+	if (sideboardArray.length === 0 || (remainingFemalePlayers.length > 0 && 
+		remainingFemalePlayers[0].skill > sideboardArray[0].skill)){
+		if (typeof remainingFemalePlayers[0] !== 'undefined'){
+			team2DList[idx].push(remainingFemalePlayers[0]);
+			remainingFemalePlayers = remainingFemalePlayers.slice(1);
+		}
+	} 
+	else if (remainingFemalePlayers.length === 0 || 
+		(sideboardArray.length > 0 && remainingFemalePlayers[0].skill <= sideboardArray[0].skill)){
+		if (typeof sideboardArray[0] !== 'undefined'){
+			team2DList[idx].push(sideboardArray[0]);
+			sideBar2DList[idx][0].female = sideBar2DList[idx][0].female.slice(1);
+		}
+	} 
+	
+	selectNewTeam.selectNewTeam();
+}
 
+selectNewTeam.resetTeamIndex();
+//check each side board and make sure they're empty
+function areSideBarsEmpty(){
+	var empty = true;
+	underscore.each(sideBar2DList, function(element, index, list){
+		if (element[0].female.length !== 0){
+			empty = false;
+		}	
+	})
+	return empty;
+}
+debugger;
 
 function divideGender(team, player){
 	if (player.gender === 'female'){
